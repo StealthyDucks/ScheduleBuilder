5b9d38931b9f3622a848c81bb668da685c15355a
Braden Schwartz <56896294+StealthyDucks@users.noreply.github.com>
2020-01-09 16:03:33 -0500

    Created the variables url, username, password and their respective getters in the DBConnect class.

--- src/schedulebuilder/DBConnect.java
+++ src/schedulebuilder/DBConnect.java
@@ -5,10 +5,30 @@
  */
 package schedulebuilder;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.api.jdbc.Statement;
+import com.mysql.cj.jdbc.result.ResultSetMetaData;
+
 /**
  *
  * @author Braden
  */
 public class DBConnect {
+    private static final String url ="";
+    private static final String username="root";
+    private static final String password="";
+    
+    public static String getUrl(){
+        return url;
+    }
+    public static String getUsername(){
+        return username;
+    }
+    public static String getPassword(){
+        return password;
+    }
     
 }
