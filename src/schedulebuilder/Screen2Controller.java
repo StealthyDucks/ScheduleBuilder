b92c0125daa039c1282d9b93203db0d558a28acb
Jared <jarrrboon@gmail.com>
2020-01-10 11:39:22 -0700

    more changes to screen 2. Still on master. Uh will swap to new branch now

--- src/schedulebuilder/Screen2Controller.java
+++ src/schedulebuilder/Screen2Controller.java
@@ -7,7 +7,12 @@ package schedulebuilder;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -15,13 +20,22 @@ import javafx.fxml.Initializable;
  * @author Jared
  */
 public class Screen2Controller implements Initializable {
-
+    public Button closeMenuButton;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Stage primaryStage = new Stage();
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         // TODO
     }    
+    public void start(Stage primaryStage){
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+    }
     
+    public void handleCloseButtonAction(ActionEvent event) {
+        Platform.exit();
+    }
 }
