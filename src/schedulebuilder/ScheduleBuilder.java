bf2b767192e815643b29b830b6475d2df9a253ae
Jared <jarrrboon@gmail.com>
2020-01-10 10:59:08 -0700

    started on screen 2. Just noticed am on master.

--- src/schedulebuilder/ScheduleBuilder.java
+++ src/schedulebuilder/ScheduleBuilder.java
@@ -19,7 +19,7 @@ public class ScheduleBuilder extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Screen2.fxml"));
         
         Scene scene = new Scene(root);
         
